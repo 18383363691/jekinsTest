486df250d62e9c65206920c9a497cb630118d079
xuli <2364033732@qq.com>
2025-09-25 21:46:03 +0800

    commit code

---
+++ src/main/java/com/example/jekinstest/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.jekinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName：TestController
+ * @Author: xuli
+ * @Date: 2025/9/25 21:38
+ * @Description: 必须描述类做什么事情, 实现什么功能
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/hello")
+    public String sayHello(String name){
+        return "welcome "+name+" to visit";
+    }
+}
